c8aa4f7b8e42c6dee609c25637891fecc704311f
Ashish Khawla <khawla@fanaticcoders.com>
2026-09-21 12:59:21 +0530

    style(admin): align security workspace

--- src/components/admin/LockedAccountsTable.tsx
+++ src/components/admin/LockedAccountsTable.tsx
@@ -43,7 +43,7 @@ export function LockedAccountsTable({ rows }: { rows: LockedAccountRow[] }) {
   }
 
   return (
-    <Table>
+    <Table scrollAreaLabel={t('security.lockedAccountsTitle')}>
       <TableHeader>
         <TableRow>
           <TableHead>{t('user.lockedAccounts.account')}</TableHead>
